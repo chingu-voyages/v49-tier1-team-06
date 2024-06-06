07d5786a892f13fded0d0f398afaca43f50670ce
maya17power <9sanchez@protonmail.com>
2024-06-05 21:45:46 -0700

    updated my prototype files.

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
+//Output of colors to page element
 const colorArray = document.querySelector('#color-array');
-//const colorSquare = document.querySelector('.card').attributes.style;
+
+//array of colors example:testing
 const colors =
     ['#FF0000',
         '#008000',
@@ -10,19 +12,17 @@ const colors =
         '#808080',
         '#C0C0C0'];
 
-//path to adjust color of card
-//console.log(colorSquare.value = 'background-color: #FF0bbb');
-
 //length of array
 const arrayLength = colors.length;
 console.log(arrayLength);
 
-//iterate of array
+//iterate of color array
+//create new div element with card and colorCard class
 const iterate = colors.map((el) => {
     const newCard = document.createElement('div');
-    newCard.className = 'card colorCard align-items-start';
+    newCard.className = 'card colorCard';
     newCard.style = `background-color: ${el}`;
     colorArray.appendChild(newCard);
 });
 
-console.log(iterate)
+//CRUD
