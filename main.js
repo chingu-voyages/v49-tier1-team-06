3b7cd196eb6c52160083187ecbc02fdb003d115d
maya17power <9sanchez@protonmail.com>
2024-06-05 21:29:16 -0700

    updated index, using cards to display color squares. updated css, setting card class to small size. updated js to iterate array of colors and set card colors for each.

---
+++ main.js
@@ -0,0 +1,28 @@
+const colorArray = document.querySelector('#color-array');
+//const colorSquare = document.querySelector('.card').attributes.style;
+const colors =
+    ['#FF0000',
+        '#008000',
+        '#0000FF',
+        '#FFFFFF',
+        '#FFFFF0',
+        '#000000',
+        '#808080',
+        '#C0C0C0'];
+
+//path to adjust color of card
+//console.log(colorSquare.value = 'background-color: #FF0bbb');
+
+//length of array
+const arrayLength = colors.length;
+console.log(arrayLength);
+
+//iterate of array
+const iterate = colors.map((el) => {
+    const newCard = document.createElement('div');
+    newCard.className = 'card colorCard container-fluid';
+    newCard.style = `background-color: ${el}`;
+    colorArray.appendChild(newCard);
+});
+
+console.log(iterate)
