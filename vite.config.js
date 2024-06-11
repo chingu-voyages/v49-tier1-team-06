11a0c00f42d91d354ceec5857cd5c1fd3c55c0da
maya17power <9sanchez@protonmail.com>
2024-06-10 21:15:38 -0700

    updated vite config js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,5 @@
+import {defineConfig} from 'vite'
 
-  // vite.config.js
-import { defineConfig } from 'vite'
-//import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/v49-tier1-team-06/'
-})
+    base: "/v49-tier1-team-06"
+});
